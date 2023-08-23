fec147c52c671c5ddb42fafbd26fe463b8b4c895
William de Angelo <wdangelo1983@gmail.com>
2023-08-23 09:46:04 -0300

    #sprint: criação rotas users / accounts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,13 @@
 import { Router } from "express"
 import { usersRoutes } from "./users.routes"
+import { accountsRoutes } from "./accounts.routes"
 
 const routes = Router()
+
+//rota para criação de usuários comuns (motoristas)
 routes.use("/users", usersRoutes)
+
+//rota para criação de contas de usuários para adiministração do sistema
+routes.use("/accounts", accountsRoutes)
 
 export { routes }
