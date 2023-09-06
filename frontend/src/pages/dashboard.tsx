501bf567768f1163bb7c001137a97358dabdd961
William de Angelo <wdangelo1983@gmail.com>
2023-09-06 14:43:56 -0300

    backup notebook prata

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,10 +1,19 @@
 import ProtectRoute from "@/components/ProtectRoute";
-
+import styles from '@/styles/pages/dashboard.module.css'
 export default function Dashboard() {
-    return (
-        <ProtectRoute>
-            <h1>Dashboard</h1>
-        </ProtectRoute>
-        
-    )
+  return (
+    <ProtectRoute>
+        <div className={styles.cardDashboard}>
+            <aside>
+                <div className={styles.cardImage}>
+                    <h1>Titulo</h1>
+                    <h2>Foto</h2>
+                </div>
+            </aside>
+        </div>
+
+
+    </ProtectRoute>
+
+  )
 }
